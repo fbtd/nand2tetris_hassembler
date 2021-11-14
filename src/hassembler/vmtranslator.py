47cf84c2dd5fc042f4d737bfff5e98fc7858f6f0
fabiano taddei <fabiano.taddei@gmail.com>
2021-11-14 07:19:44 +0100

    Translate push instructions

--- src/hassembler/vmtranslator.py
+++ src/hassembler/vmtranslator.py
@@ -2,8 +2,43 @@ from hassembler.parser import VmParser
 import pathlib
 import sys
 
-def encode(instruction):
-    return ''
+_segment_to_registers = dict(local='LCL', argument='ARG', this='THIS', that='THAT')
+
+def encode_segment_to_d(segment, index, static_prefix=None):
+    register = None
+    if segment in _segment_to_registers:
+        register = _segment_to_registers[segment]
+        return f'@{index}, D=A, @{register}, A=M, A=A+D, D=M'.split(', ')
+    if segment == 'pointer':
+        if index == '0': register='THIS'
+        elif index == '1': register='THAT'
+        else: raise ValueError(f'index {index} invalid for "pointer" segment')
+    if segment == 'temp':
+        register = f'R{int(index) + 5}'
+    if segment == 'static':
+        if static_prefix is None:
+            raise RuntimeError('missing static prefix')
+        register = f'{static_prefix}.{index}'
+    return [f'@{register}', 'D=A']
+
+def encode_constant_to_d(constant):
+    return [f'@{constant}', 'D=A']
+
+_push_d = '@SP, A=M, M=D, @SP, M=M+1'.split(', ')
+
+def encode(vm_instruction, static_prefix=None):
+    asm_instruction = [f'// {vm_instruction}']
+    if vm_instruction['operation'] == 'push':
+        if 'constant' in vm_instruction:
+            asm_instruction.extend(encode_constant_to_d(vm_instruction['constant']))
+        elif 'segment' in vm_instruction:
+            asm_instruction.extend(encode_segment_to_d(vm_instruction['segment'],
+                vm_instruction['index'], static_prefix))
+        asm_instruction.extend(_push_d)
+    asm_instruction.append('/////////////')
+    asm_instruction.append('')
+    return asm_instruction
+
 
 def main(source_file=None):
     if source_file is None:
@@ -11,9 +46,10 @@ def main(source_file=None):
     parser = VmParser(source_file)
 
     dest_file = pathlib.Path(source_file).with_suffix('.asm')
+    basename = pathlib.Path(source_file).with_suffix('')
     with open(dest_file, 'w') as fout:
         for instruction in parser:
-            encoded_instruction = encode(instruction)
+            encoded_instruction = encode(instruction, basename)
             fout.write(encoded_instruction)
             fout.write('\n')
 
--- tests/test_vmtranslator.py
+++ tests/test_vmtranslator.py
@@ -1,39 +1,22 @@
 import pytest
 from pathlib import Path
-import hassembler.hassembler
+import hassembler.vmtranslator as t
 from mockparser import MockParser
 
 @pytest.mark.parametrize('instruction,expected', [
-        ({'instruction_type': 'A', 'value': '0'},    '0000000000000000'),
-        ({'instruction_type': 'A', 'value': '1001'}, '0000001111101001'),
-        ({'instruction_type': 'C', 'dest': 'M', 'comp': '1'}, '1110111111001000'),
-        ({'instruction_type': 'C', 'dest': 'M', 'comp': 'M+1'}, '1111110111001000'),
-        ({'instruction_type': 'C', 'comp': 'D', 'jump': 'JGT'}, '1110001100000001'),
-        ({'instruction_type': 'C', 'comp': '0', 'jump': 'JMP'}, '1110101010000111'),
-        ({'instruction_type': 'C', 'dest': 'M', 'comp': 'D'}, '1110001100001000'),
-        ({'instruction_type': 'C', 'dest': 'D', 'comp': 'D+1', 'jump': 'JLT'}, '1110011111010100')
+    ({'operation': 'push', 'constant': '510'}, '@510, D=A, @SP, A=M, M=D, @SP, M=M+1'),
+    ({'operation': 'push', 'segment': 'local', 'index': '3'},
+        '@3, D=A, @LCL, A=M, A=A+D, D=M, @SP, A=M, M=D, @SP, M=M+1'),
+    ({'operation': 'push', 'segment': 'pointer', 'index': '0'},
+        '@THIS, D=A, @SP, A=M, M=D, @SP, M=M+1'),
+    ({'operation': 'push', 'segment': 'pointer', 'index': '1'},
+        '@THAT, D=A, @SP, A=M, M=D, @SP, M=M+1'),
+    ({'operation': 'push', 'segment': 'temp', 'index': '3'},
+        '@R8, D=A, @SP, A=M, M=D, @SP, M=M+1'),
+    ({'operation': 'push', 'segment': 'static', 'index': '3'},
+        '@prog.3, D=A, @SP, A=M, M=D, @SP, M=M+1'),
     ])
 def test_encode(instruction, expected):
-    assert hassembler.hassembler.encode(instruction) == expected
-
-def test_make_symbol_table():
-    reference_instructions = [
-        {'instruction_line': 0, 'instruction_type': 'A', 'value': '0'},
-        {'instruction_line': 1, 'instruction_type': 'A', 'symbol': 'first_symbol'},
-        {'instruction_line': 2, 'instruction_type': 'LABEL', 'label': 'SECOND_SYMBOL'},
-        {'instruction_line': 2, 'instruction_type': 'A', 'value': '0'},
-        {'instruction_line': 3, 'instruction_type': 'A', 'symbol': 'SECOND_SYMBOL'},
-    ]
-    mparser = MockParser(reference_instructions)
-
-    symbol_table = hassembler.hassembler.make_symbol_table(mparser)
-    assert symbol_table['R0'] == 0
-    assert symbol_table['R1'] == 1
-    assert symbol_table['R15'] == 15
-    assert symbol_table['SCREEN'] == 16384
-    assert symbol_table['first_symbol'] == 16
-    assert symbol_table['SECOND_SYMBOL'] == 2
-
-
-def test_main():
-    assert True
+    expected_list = expected.split(', ')
+    translated_list = [l for l in t.encode(instruction, 'prog') if (not l.startswith('//') and l)]
+    assert translated_list == expected_list
