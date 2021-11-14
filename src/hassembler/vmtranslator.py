198250c7cad438f9ddfac2d3ccc4aa76dc453dab
fabiano taddei <fabiano.taddei@gmail.com>
2021-11-14 10:51:46 +0100

    Translate pop instructions

--- src/hassembler/vmtranslator.py
+++ src/hassembler/vmtranslator.py
@@ -3,6 +3,14 @@ import pathlib
 import sys
 
 _segment_to_registers = dict(local='LCL', argument='ARG', this='THIS', that='THAT')
+_pointer_index_to_register = {'0': 'THIS', '1': 'THAT'}
+
+def encode_target_address_to_R13(segment, index, static_prefix=None):
+    register = None
+    if segment in _segment_to_registers:
+        register = _segment_to_registers[segment]
+        return f'@{index}, D=A, @{register}, A=M, A=A+D, D=A, @R13, M=D'.split(', ')
+    return []
 
 def encode_segment_to_d(segment, index, static_prefix=None):
     register = None
@@ -10,9 +18,7 @@ def encode_segment_to_d(segment, index, static_prefix=None):
         register = _segment_to_registers[segment]
         return f'@{index}, D=A, @{register}, A=M, A=A+D, D=M'.split(', ')
     if segment == 'pointer':
-        if index == '0': register='THIS'
-        elif index == '1': register='THAT'
-        else: raise ValueError(f'index {index} invalid for "pointer" segment')
+        register = _pointer_index_to_register[index]
     if segment == 'temp':
         register = f'R{int(index) + 5}'
     if segment == 'static':
@@ -25,16 +31,36 @@ def encode_constant_to_d(constant):
     return [f'@{constant}', 'D=A']
 
 _push_d = '@SP, A=M, M=D, @SP, M=M+1'.split(', ')
+_pop_to_d = '@SP, A=M, D=M, @SP M=M-1'.split(', ')
 
 def encode(vm_instruction, static_prefix=None):
     asm_instruction = [f'// {vm_instruction}']
-    if vm_instruction['operation'] == 'push':
+    operator = vm_instruction['operation']
+    segment = vm_instruction.get('segment')
+    index = vm_instruction.get('index')
+    if operator == 'push':
         if 'constant' in vm_instruction:
             asm_instruction.extend(encode_constant_to_d(vm_instruction['constant']))
-        elif 'segment' in vm_instruction:
-            asm_instruction.extend(encode_segment_to_d(vm_instruction['segment'],
-                vm_instruction['index'], static_prefix))
+        elif segment is not None:
+            asm_instruction.extend(encode_segment_to_d(segment, index, static_prefix))
         asm_instruction.extend(_push_d)
+    elif operator == 'pop':
+        if segment in _segment_to_registers:
+            asm_instruction.extend(encode_target_address_to_R13(segment, index, static_prefix))
+            asm_instruction.extend(_pop_to_d)
+            asm_instruction.extend(['@R13', 'M=D'])
+        else:
+            register = None
+            asm_instruction.extend(_pop_to_d)
+            if segment == 'pointer':
+                register = _pointer_index_to_register[index]
+            elif segment == 'temp':
+                register = f'R{int(index) + 5}'
+            elif segment == 'static':
+                if static_prefix is None:
+                    raise RuntimeError('missing static prefix')
+                register = f'{static_prefix}.{index}'
+            asm_instruction.extend([f'@{register}', 'M=D'])
     asm_instruction.append('/////////////')
     asm_instruction.append('')
     return asm_instruction
@@ -49,9 +75,9 @@ def main(source_file=None):
     basename = pathlib.Path(source_file).with_suffix('')
     with open(dest_file, 'w') as fout:
         for instruction in parser:
-            encoded_instruction = encode(instruction, basename)
-            fout.write(encoded_instruction)
-            fout.write('\n')
+            for encoded_instruction in encode(instruction, basename):
+                fout.write(encoded_instruction)
+                fout.write('\n')
 
 if __name__ == '__main__':
     main()
--- tests/test_vmtranslator.py
+++ tests/test_vmtranslator.py
@@ -4,6 +4,7 @@ import hassembler.vmtranslator as t
 from mockparser import MockParser
 
 @pytest.mark.parametrize('instruction,expected', [
+# push
     ({'operation': 'push', 'constant': '510'}, '@510, D=A, @SP, A=M, M=D, @SP, M=M+1'),
     ({'operation': 'push', 'segment': 'local', 'index': '3'},
         '@3, D=A, @LCL, A=M, A=A+D, D=M, @SP, A=M, M=D, @SP, M=M+1'),
@@ -15,6 +16,19 @@ from mockparser import MockParser
         '@R8, D=A, @SP, A=M, M=D, @SP, M=M+1'),
     ({'operation': 'push', 'segment': 'static', 'index': '3'},
         '@prog.3, D=A, @SP, A=M, M=D, @SP, M=M+1'),
+# pop
+    ({'operation': 'pop', 'segment': 'local', 'index': '3'},
+        '@3, D=A, @LCL, A=M, A=A+D, D=A, @R13, M=D, @SP, A=M, D=M, @SP M=M-1, @R13, M=D'),
+         ########## &target to R13 ###############  ###### --stack to D ####  ## set ##
+    ({'operation': 'pop', 'segment': 'pointer', 'index': '0'},
+        '@SP, A=M, D=M, @SP M=M-1, @THIS, M=D'),
+         ##### --stack to D ##### 
+    ({'operation': 'pop', 'segment': 'pointer', 'index': '1'},
+        '@SP, A=M, D=M, @SP M=M-1, @THAT, M=D'),
+    ({'operation': 'pop', 'segment': 'temp', 'index': '3'},
+        '@SP, A=M, D=M, @SP M=M-1, @R8, M=D'),
+    ({'operation': 'pop', 'segment': 'static', 'index': '3'},
+        '@SP, A=M, D=M, @SP M=M-1, @prog.3, M=D'),
     ])
 def test_encode(instruction, expected):
     expected_list = expected.split(', ')
